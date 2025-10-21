54dc42e2f060df5bda89ba48884241b42b8b3b0e
Maria Eduarda <eduardabritto2014@gmail.com>
2025-10-21 03:13:42 -0300

    Update Usuario class
    Refactor Usuario class to include 'id' attribute and remove redundant methods.

--- models/Usuario.py
+++ models/Usuario.py
@@ -1,19 +1,4 @@
 class Usuario:
-    def __init__(self, nome, cpf, data_nascimento):
-        self.nome = nome
-        self.cpf = cpf
-        self.data_nascimento = data_nascimento
-        
-    def to_json(self):
-        return {
-            "nome": self.nome,
-            "cpf": self.cpf,
-            "data_nascimento": self.data_nascimento
-        }
-
-
-
-class Usuario:
     def __init__(self, id, nome, cpf, data_nascimento):
         self.id = id
         self.nome = nome
